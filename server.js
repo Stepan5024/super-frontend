ed8d312719617516c31aea95c05ae14a0254de28
Степан <bokarevstepan1@yandex.ru>
2021-06-22 17:48:23 +0300

    Сервер и html страница

---
+++ server.js
@@ -0,0 +1,17 @@
+'use strict';
+const http = require('http');
+const fs = require('fs'); //читаем файл
+const server = http.createServer((request, response) => {
+    console.log(request.method,request.url); // на любую команду/ запрос к серверу вывести сообщение в консоль
+    if(request.url == '/style.css'){
+        const css = fs.readFileSync('style.css', 'utf8');
+        response.end(css);//обработали запрос и его можно завершить
+    }else{
+
+    }
+    const html = fs.readFileSync('index.html', 'utf8');
+    response.end(html);//обработали запрос и его можно завершить
+});
+server.listen(3000);// запуск сервера
+console.log('Server started!');
+
